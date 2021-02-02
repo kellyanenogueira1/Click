94fd856b64ff1c80f5865e1eed1d582bd7673154
Kellyane Nogueira <kellyanenogueira1@gmail.com>
2021-02-02 15:29:51 -0300

    Implementando gestures

--- Click/Controller/ViewController.swift
+++ Click/Controller/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor.white
+        gestures()
+        
     }
     
     func isNewUser() -> Bool{
@@ -19,11 +22,30 @@ class ViewController: UIViewController {
     }
     
     override func viewWillLayoutSubviews() {
-        if isNewUser(){
+        if isNewUser() {
             let onboarding = UIHostingController(rootView: ContentView())
             self.present(onboarding, animated: true, completion: nil)
         }
         UserDefaults.standard.set(true, forKey: "isNewUser")
     }
+    
+    @objc func callToFriend(recognizer: UITapGestureRecognizer) {
+        print("triple tap")
+    }
+    
+    @objc func callToPolice(recognizer: UILongPressGestureRecognizer) {
+        print("press")
+    }
+    
+    func gestures() {
+        let press = UILongPressGestureRecognizer(target: self, action: #selector(executePress))
+        press.minimumPressDuration = 3.0
+        let tap = UITapGestureRecognizer(target: self, action: #selector(executeTap))
+        tap.numberOfTapsRequired = 3
+
+        view.addGestureRecognizer(press)
+        view.addGestureRecognizer(tap)
+    }
+    
 
 }
